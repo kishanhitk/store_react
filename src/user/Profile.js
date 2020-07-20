d969e4b97fd653ea2e380946a1faa73a6cb94345
Kishan Kumar <kishansharma1231@gmail.com>
2020-07-20 14:41:45 +0530

    Added Dashboard page

---
+++ src/user/Profile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Base from '../core/Base'
+const ProfilePage = () => {
+    return (
+        <Base title="Profile">
+            <h1>This is Profile Page.</h1>
+        </Base>
+    )
+}
+
+export default ProfilePage
