1b10f4457a0d16442086c24ed0e62741814639ec
Kishan Kumar <kishansharma1231@gmail.com>
2020-09-27 16:42:19 +0530

    Created left side of Admin Dashboard

--- src/user/AdminDashBoard.js
+++ src/user/AdminDashBoard.js
@@ -1,9 +1,52 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { isAuthenticated } from "../auth/helper";
 import Base from "../core/Base";
 const AdminDashboard = () => {
+  const {
+    user: { email, name, role },
+  } = isAuthenticated();
+
+  const adminLeftSide = () => {
+    return (
+      <div className="card">
+        <h4 className="card-header bg-dark text-white">Admin Navigation</h4>
+        <ul className="list-group">
+          <li className="list-group-item">
+            <Link className="nav-link text-success" to="/admin/create/category">
+              Create Categories
+            </Link>
+          </li>
+          <li className="list-group-item">
+            <Link className="nav-link text-success" to="/admin/create/product">
+              Create Products
+            </Link>
+          </li>
+          <li className="list-group-item">
+            <Link className="nav-link text-success" to="/admin/products">
+              Manage Products
+            </Link>
+          </li>
+          <li className="list-group-item">
+            <Link className="nav-link text-success" to="/admin/orders">
+              Manage Orders
+            </Link>
+          </li>
+        </ul>
+      </div>
+    );
+  };
+  const adminRightSide = () => {};
   return (
-    <Base title="Admin Dashbaord">
-      <h1>This is a AdminDashboard Page.</h1>
+    <Base
+      title="Admin Dashbaord"
+      description="Manage all your products here."
+      className="container bg-success p-4"
+    >
+      <div className="row">
+        <div className="col-3">{adminLeftSide()}</div>
+        <div className="col-9">{adminRightSide()}</div>
+      </div>
     </Base>
   );
 };
