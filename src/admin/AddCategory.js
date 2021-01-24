348e5dda92107c9ffc7d08124a7e460b72d99860
kishanhitk <kishansharma1231@gmail.com>
2021-01-24 20:26:06 +0530

    Add category to DB

--- src/admin/AddCategory.js
+++ src/admin/AddCategory.js
@@ -2,25 +2,62 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { isAuthenticated } from "../auth/helper";
 import Base from "../core/Base";
+import { createCategory } from "./helper/adminapicall";
 
 export default function AddCategory() {
-  const [name, setName] = useState("initialState");
+  const [name, setName] = useState("");
   const [error, setError] = useState(false);
   const [success, setSuccess] = useState(false);
   const { user, token } = isAuthenticated();
+  const handleChange = (event) => {
+    setError("");
+    setName(event.target.value);
+  };
+  const SuccessMessage = () => {
+    if (success) {
+      return <h4 className="text-success">Category Created Successfully</h4>;
+    } else return null;
+  };
+  const ErrorMessage = () => {
+    if (error) {
+      return <h4 className="text-danger">Failed Calling Category</h4>;
+    } else return null;
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setError("");
+    setSuccess(false);
+
+    //Backend Request Fired
+    createCategory(user._id, token, { name }).then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setError("");
+        setName("");
+        setSuccess(true);
+      }
+    });
+  };
   const myForm = () => {
     return (
       <form>
+        <SuccessMessage />
+        <ErrorMessage />
         <div className="form-group">
           <p className="lead">Enter the Category</p>
           <input
             type="text"
             autoFocus
             placeholder="For ex. Summer"
+            onChange={handleChange}
+            value={name}
             required
             className="form-control my-3"
           />
-          <button className="btn btn-outline btn-info">Create Category</button>
+          <button onClick={handleSubmit} className="btn btn-outline btn-info">
+            Create Category
+          </button>
         </div>
       </form>
     );
--- src/admin/helper/adminapicall.js
+++ src/admin/helper/adminapicall.js
@@ -1,17 +1,18 @@
 const { API } = require("../../backend");
 
-export const createCategory = (userId, token, category) => {
-  return fetch(`${API}/category/create/${userId}`, {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify(category),
-  })
-    .then((response) => {
-      return response.json();
-    })
-    .catch((err) => console.log(err));
+export const createCategory = async (userId, token, category) => {
+  try {
+    const response = await fetch(`${API}/category/create/${userId}`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(category),
+    });
+    return await response.json();
+  } catch (err) {
+    return console.log(err);
+  }
 };
